90c449b1bdfd538eab2dd80b4a85d38ae5a850a7
creyente1897 <ayushsrivastava1897@gmail.com>
2019-09-27 02:37:50 +0530

    Updates

---
+++ precache-manifest.7c34e54899a0619d8a2b8441e359fb72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9b140876e638837bd6d83e27417770",
+    "url": "/robos-rolodex/index.html"
+  },
+  {
+    "revision": "11014c64ac5e9b9a5c4b",
+    "url": "/robos-rolodex/static/css/main.c27f1209.chunk.css"
+  },
+  {
+    "revision": "b351e067063348c0f181",
+    "url": "/robos-rolodex/static/js/2.c96dec80.chunk.js"
+  },
+  {
+    "revision": "11014c64ac5e9b9a5c4b",
+    "url": "/robos-rolodex/static/js/main.b716129c.chunk.js"
+  },
+  {
+    "revision": "6eb5ab97a626ebd36008",
+    "url": "/robos-rolodex/static/js/runtime-main.7bc6885a.js"
+  }
+]);
